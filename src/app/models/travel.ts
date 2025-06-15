ebeaaf0164d540e309e62ab0864d4c273df80fd2
nnsw1 <krevetkaplohaa@gmail.com>
2025-06-15 07:57:24 +0300

    flags

--- src/app/models/travel.ts
+++ src/app/models/travel.ts
@@ -2,7 +2,9 @@ export interface ITravel {
   id: string;
   country: string;
   city: string;
+  short_description: string;
   description: string;
+  flag: string;
   img: string;
   year: number;
   featured: boolean;
