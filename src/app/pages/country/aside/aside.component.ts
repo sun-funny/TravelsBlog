f5458c62f067f309c01679f1a5e108564adf61aa
nnsw1 <krevetkaplohaa@gmail.com>
2025-06-16 18:56:04 +0300

    aside

--- src/app/pages/country/aside/aside.component.ts
+++ src/app/pages/country/aside/aside.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IPoint } from '../../../models/point';
 
 @Component({
   selector: 'app-aside',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aside.component.scss']
 })
 export class AsideComponent implements OnInit {
+  @Input() points: IPoint[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  scrollToPoint(pointId: string): void {
+  const element = document.getElementById(pointId);
+  if (element) {
+    const headerHeight = 82;
+    const elementPosition = element.getBoundingClientRect().top;
+    const offsetPosition = elementPosition + window.pageYOffset - headerHeight;
+    
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth'
+    });
+    
+    history.replaceState(null, '', `${location.pathname}#${pointId}`);
+  }
+}
 }
