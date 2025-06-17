450d45f4a79dd6f38bd780c0798e6b4fd0afca07
nnsw1 <krevetkaplohaa@gmail.com>
2025-06-17 18:59:07 +0300

    header

--- src/app/pages/ui/header/header.component.ts
+++ src/app/pages/ui/header/header.component.ts
@@ -1,6 +1,4 @@
-import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
-import { cn } from '../../lib/utils';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,65 +6,9 @@ import { cn } from '../../lib/utils';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  @Input() className = '';
-  
   navItems = [
     { text: "ГЛАВНАЯ", width: "165px", route: "/main" },
     { text: "НАШИ ПУТЕШЕСТВИЯ", width: "390px", route: "/travels" },
     { text: "FAQ", width: "334px", route: "/faq" },
   ];
-
-  constructor(private router: Router) {}
-
-  navigateTo(route: string): void {
-    this.router.navigate([route]);
-  }
-
-  get headerClasses(): string {
-    return cn('max-w-none w-full flex justify-center', this.className);
-  }
-}
-
-@Component({
-  selector: 'app-header-list',
-  templateUrl: './header-list.component.html',
-  styleUrls: ['./header.component.scss']
-})
-export class HeaderListComponent {
-  @Input() className = '';
-  
-  get listClasses(): string {
-    return cn('flex gap-4 justify-center', this.className);
-  }
-}
-
-@Component({
-  selector: 'app-header-item',
-  templateUrl: './header-item.component.html',
-  styleUrls: ['./header.component.scss']
-})
-export class HeaderItemComponent {
-  @Input() className = '';
-  
-  get itemClasses(): string {
-    return cn('', this.className);
-  }
-}
-
-@Component({
-  selector: 'app-header-link',
-  templateUrl: './header-link.component.html',
-  styleUrls: ['./header.component.scss']
-})
-export class HeaderLinkComponent {
-  @Input() className = '';
-  @Input() width = '';
-  @Input() route = '';
-  
-  get linkClasses(): string {
-    return cn(
-      'flex items-center justify-center font-dela-gothic text-white text-2xl transition-colors hover:bg-[#6da7d3]',
-      this.className
-    );
-  }
 }
--- src/app/pages/ui/ui.module.ts
+++ src/app/pages/ui/ui.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { CardComponent, CardContentComponent, CardHeaderComponent } from './card/card.component';
-import { HeaderComponent, HeaderListComponent, HeaderItemComponent, HeaderLinkComponent } from './header/header.component';
+import { HeaderComponent} from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 
@@ -13,9 +13,6 @@ import { RouterModule } from '@angular/router';
     CardContentComponent,
     CardHeaderComponent,
     HeaderComponent,
-    HeaderListComponent,
-    HeaderItemComponent,
-    HeaderLinkComponent,
     FooterComponent
   ],
   imports: [
@@ -28,9 +25,6 @@ import { RouterModule } from '@angular/router';
     CardContentComponent,
     CardHeaderComponent,
     HeaderComponent,
-    HeaderListComponent,
-    HeaderItemComponent,
-    HeaderLinkComponent,
     FooterComponent
   ]
 })
